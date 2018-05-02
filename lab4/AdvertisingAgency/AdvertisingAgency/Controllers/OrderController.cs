3ad1fe33878c94b251dcbdc06c9cb925f6210f4b
Raman Samuseu <roman.samusev@mail.ru>
2018-05-02 03:20:17 +0300

    lab4 done

---
+++ lab4/AdvertisingAgency/AdvertisingAgency/Controllers/OrderController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AdvertisingAgency.Models;
+using Microsoft.Extensions.Caching.Memory;
+using AdvertisingAgency.Filtres;
+using Newtonsoft.Json;
+
+namespace AdvertisingAgency.Controllers
+{
+    [SetToSession]
+    public class OrderController : Controller
+    {
+        public enum SortState
+        {
+            DateOrderAsc,    // по дате договора по возрастанию
+            DateOrderDesc,   // по дате договра по убыванию
+            PeriodAsc,     // по периоду по возрастанию
+            PeriodDesc,   // по периоду по убыванию
+            LocationAsc, // по локации по возрастанию
+            LocationDesc, // по локации по убыванию
+            PriceAsc,        // по цене по убыванию
+            PriceDesc       // по цене по убыванию
+        }
+
+        readonly IMemoryCache _memoryCache;
+        readonly adAgencyContext _context;
+
+        public OrderController(IMemoryCache memoryCache, adAgencyContext context)
+        {
+            _memoryCache = memoryCache;
+            _context = context;
+        }
+
+        [ResponseCache(CacheProfileName = "Caching")]
+        public IActionResult Order(string DateOrder, SortState sortOrder = SortState.DateOrderAsc )
+        {
+            Orders ordersFromMemoryCashe = (Orders)_memoryCache.Get(Request.Path.Value.ToLower());
+            ViewBag.ordFromMemory = ordersFromMemoryCashe;
+
+            if (Request.Cookies["orderSession"] != null)
+            {
+                Orders orderSession = JsonConvert.DeserializeObject<Orders>(Request.Cookies["orderSession"].ToString());
+                ViewBag.orderSession = orderSession;
+            }
+
+            if (HttpContext.Session.Get("orderSortSession") != null)
+            {
+                sortOrder = JsonConvert.DeserializeObject<SortState>(HttpContext.Session.GetString("orderSortSession"));
+            }
+
+            IQueryable<Orders> orders = _context.Orders;
+            DateTime dateorder = Convert.ToDateTime(DateOrder);
+            if (DateOrder != null)
+            {
+                orders = orders.Where(o => o.DateOrder.Date.Equals(dateorder));
+            }
+
+            ViewBag.DateOrderSort = sortOrder == SortState.DateOrderAsc ? SortState.DateOrderDesc : SortState.DateOrderAsc;
+            ViewBag.OrderPeriodSort = sortOrder == SortState.PeriodAsc ? SortState.PeriodDesc : SortState.PeriodAsc;
+            ViewBag.OrderLocationSort = sortOrder == SortState.LocationAsc ? SortState.LocationDesc : SortState.LocationAsc;
+            ViewBag.OrderCostSort = sortOrder == SortState.PriceAsc ? SortState.PriceDesc : SortState.PriceAsc;
+
+            switch (sortOrder)
+            {
+                case SortState.DateOrderAsc:
+                    orders = orders.OrderBy(o => o.DateOrder);
+                    break;
+                case SortState.DateOrderDesc:
+                    orders = orders.OrderByDescending(o => o.DateOrder);
+                    break;
+                case SortState.PeriodAsc:
+                    orders = orders.OrderBy(o => o.DateBegin);
+                    break;
+                case SortState.PeriodDesc:
+                    orders = orders.OrderByDescending(o => o.DateEnd);
+                    break;
+                case SortState.LocationAsc:
+                    orders = orders.OrderBy(o => o.Location);
+                    break;
+                case SortState.LocationDesc:
+                    orders = orders.OrderByDescending(o => o.Location);
+                    break;
+                case SortState.PriceAsc:
+                    orders = orders.OrderBy(o => o.OrderCost);
+                    break;
+                case SortState.PriceDesc:
+                    orders = orders.OrderByDescending(o => o.OrderCost);
+                    break;
+                default:
+                    orders = orders.OrderBy(o => o.DateOrder);
+                    break;
+            }
+
+            ViewBag.orders = orders.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddOrder(Orders order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return RedirectToAction("Order");
+        }
+    }
+}
---
+++ lab4/AdvertisingAgency/AdvertisingAgency/Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertisingAgency.Models
+{
+    public class Orders
+    {
+        public Orders() { }
+
+        public Orders(DateTime DateOrder, DateTime DateBegin, DateTime DateEnd, string Location, int OrderCost)
+        {
+            this.DateOrder = DateOrder;
+            this.DateBegin = DateBegin;
+            this.DateEnd = DateEnd;
+            this.Location = Location;
+            this.OrderCost = OrderCost;
+        }
+
+        public int OrdersId { get; set; }
+        public DateTime DateOrder { get; set; }
+        public DateTime DateBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Location { get; set; }
+        public int OrderCost { get; set; }
+        public bool PaymentStatus { get; set; }
+        public int? EmployeeId { get; set; }
+        public int? CustomersId { get; set; }
+        public int AdvedirsmentsId { get; set; }
+        public virtual Advedirsments Advedirsments { get; set; }
+        public virtual Customers Customers { get; set; }
+    }
+}
