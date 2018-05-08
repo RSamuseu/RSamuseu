0437e4a74e4b58d23eb3adac4ae86cd10263c87e
Raman Samuseu <roman.samusev@mail.ru>
2018-05-08 16:55:41 +0300

    lab6 solved

---
+++ lab6/AdAgencyWebAPI/AdAgencyWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AdAgencyWebAPI.Data;
+using AdAgencyWebAPI.Models;
+using AdAgencyWebAPI.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdAgencyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly adAgencyContext _context;
+
+        public HomeController(adAgencyContext context)
+        {
+            _context = context;
+        }
+
+        //GET api/values
+        [HttpGet]
+        [Produces("application/json")]
+        public List<OrderViewModels> Get()
+        {
+            var item = _context.Orders.Include(t => t.Advedirsments).Select(o =>
+             new OrderViewModels
+             {
+                 OrdersId = o.OrdersId,
+                 DateOrder = o.DateOrder,
+                 Location = o.Location,
+                 OrderCost = o.OrderCost,
+             });
+            return item.OrderBy(o => o.OrdersId).ToList();
+        }
+
+        // GET api/values
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = _context.Orders.FirstOrDefault(o => o.OrdersId == id);
+            if (item == null)
+                return NotFound();
+            return new ObjectResult(item);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Orders order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return Ok(order);
+        }
+
+        // PUT api/values
+        public IActionResult Put([FromBody]Orders order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+
+            if (!_context.Orders.Any(o => o.OrdersId == order.OrdersId))
+            {
+                return NotFound();
+            }
+
+            _context.Update(order);
+            _context.SaveChanges();
+
+            return Ok(order);
+        }
+
+        // DELETE api/values
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Orders order = _context.Orders.FirstOrDefault(o => o.OrdersId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+            return Ok(order);
+        }
+    }
+}
---
+++ lab6/AdAgencyWebAPI/AdAgencyWebAPI/ViewModels/OrderViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdAgencyWebAPI.ViewModels
+{
+    public class OrderViewModels
+    {
+        public int OrdersId { get; set; }
+        public DateTime DateOrder { get; set; }
+        public string Location { get; set; }
+        public int OrderCost { get; set; }
+    }
+}
