1eed7de7828653493cf408a4712fca3f260e2239
Raman Samuseu <roman.samusev@mail.ru>
2018-05-12 20:21:26 +0300

    lab6 update

---
+++ lab6/AdAgencyWebAPI/AdAgencyWebAPI/Data/adAgencyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdAgencyWebAPI.Models; 
+
+namespace AdAgencyWebAPI.Data
+{
+    public class adAgencyContext:DbContext
+    {
+        public DbSet<Advedirsments> Advedirsments { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+        public adAgencyContext(DbContextOptions<adAgencyContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ lab6/AdAgencyWebAPI/AdAgencyWebAPI/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AdAgencyWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdAgencyWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<adAgencyContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SQLConnection")));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, adAgencyContext context)
+        {
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseMvc();
+        }
+    }
+}
